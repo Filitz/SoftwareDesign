063ba42b22ff643719617f1cb45bc949f97d5c4f
Filitz <54859980+Filitz@users.noreply.github.com>
2020-01-23 08:05:01 -0500

    commit #2

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentDemo.java
@@ -13,6 +13,15 @@ public class StudentDemo
 {
   private String studentID;
   private String name;
+  private double Score;
+
+    public double getScore() {
+        return Score;
+    }
+
+    public void setScore(double Score) {
+        this.Score = Score;
+    }
   
   /**
    * A constructor to pass in the given ID and then name
